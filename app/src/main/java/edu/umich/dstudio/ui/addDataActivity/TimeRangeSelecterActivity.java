2899c22682b50e8374be3e8e137a64577dba7c6e
Shriti-UCI <shriti.raj13@gmail.com>
2016-03-30 13:43:59 -0400

    time range picker additions, incomplete still

---
+++ app/src/main/java/edu/umich/dstudio/ui/addDataActivity/TimeRangeSelecterActivity.java
@@ -0,0 +1,22 @@
+package edu.umich.dstudio.ui.addDataActivity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.umich.dstudio.R;
+
+
+public class TimeRangeSelecterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_range_selecter);
+    }
+
+
+
+}
